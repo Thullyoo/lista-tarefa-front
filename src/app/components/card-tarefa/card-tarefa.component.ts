51a8e4ba2d570b02673d13d0ca9a153b2ed9d07d
Thullyoo <thullyofilho@gmail.com>
2024-11-01 12:11:12 -0300

    feat: import do common module para ngif

--- src/app/components/card-tarefa/card-tarefa.component.ts
+++ src/app/components/card-tarefa/card-tarefa.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Tarefa } from '../../type/tarefa-type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-tarefa',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-tarefa.component.html',
   styleUrl: './card-tarefa.component.css'
 })
