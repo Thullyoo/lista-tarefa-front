b7d60ad01a12126dbbd42739e1a5f039d18701af
Thullyoo <thullyofilho@gmail.com>
2024-11-04 11:55:01 -0300

    feat: formatando Date para String

--- src/app/components/card-tarefa/card-tarefa.component.ts
+++ src/app/components/card-tarefa/card-tarefa.component.ts
@@ -1,5 +1,5 @@
-import { Component, input } from '@angular/core';
-import { Tarefa } from '../../type/tarefa-type';
+import { Tarefa } from './../../type/tarefa-type';
+import { Component, input, type InputSignal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,5 +11,15 @@ import { CommonModule } from '@angular/common';
 })
 export class CardTarefaComponent {
 
-  public tarefa = input.required<Tarefa>();
+  public tarefa: InputSignal<Tarefa> = input.required<Tarefa>();
+
+  formatDate(data: any): string {
+    const dateObject = (data instanceof Date) ? data : new Date(data);
+    return dateObject.toLocaleDateString("pt-BR", {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    });
+  }
+
 }
