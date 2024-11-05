3f5106a46147bf1a3ce72de07b3afd5723bd67a7
Thullyoo <thullyofilho@gmail.com>
2024-11-05 10:12:32 -0300

    feat: adicionado metodo para requisição de troca de ordem

--- src/app/services/tarefa-service.service.ts
+++ src/app/services/tarefa-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import type { Tarefa } from '../type/tarefa-type';
 import type { Observable } from 'rxjs';
+import type { TarefaOrdem } from '../type/tarefa-ordem-type';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,16 @@ export class TarefaServiceService {
 
   private url: string = "http://localhost:8080/tarefas";
 
-  getTarefa(): Observable<Tarefa[]>{
+  listarTarefa(): Observable<Tarefa[]>{
     return this.httpClient.get<Tarefa[]>(this.url);
+  }
+  
+  editarOrdemTarefas(listOrdem: TarefaOrdem[]){
+    const payload = { ordemList: listOrdem };
+    this.httpClient.put<Tarefa[]>(this.url, payload).subscribe({
+      next: (response) => console.log("Requisição bem-sucedida", response),
+        error: (error) => console.error("Erro ao fazer a requisição", error)
+    });
   }
   
 }
