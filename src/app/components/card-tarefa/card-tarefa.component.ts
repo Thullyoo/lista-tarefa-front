22acf12ab5847f7eede41c877cbdfb91eb4114c1
Thullyoo <thullyofilho@gmail.com>
2024-11-07 12:38:18 -0300

    feat: adicionado form para excluir e editar tarefas

--- src/app/components/card-tarefa/card-tarefa.component.ts
+++ src/app/components/card-tarefa/card-tarefa.component.ts
@@ -1,17 +1,48 @@
 import { Tarefa } from './../../type/tarefa-type';
-import { Component, input, type InputSignal } from '@angular/core';
+import { Component, input, type InputSignal, type OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormEditarComponent } from '../form-editar/form-editar.component';
+import { FormExcluirComponent } from "../form-excluir/form-excluir.component";
 
 @Component({
   selector: 'app-card-tarefa',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormEditarComponent, FormExcluirComponent],
   templateUrl: './card-tarefa.component.html',
   styleUrl: './card-tarefa.component.css'
 })
-export class CardTarefaComponent {
+export class CardTarefaComponent implements OnInit{
 
   public tarefa: InputSignal<Tarefa> = input.required<Tarefa>();
+  
+  tarefa_id: number = 0;
+
+  ngOnInit(): void {
+    this.tarefa_id = this.tarefa().id
+  }
+
+
+  displayFormExcluir = false;
+
+  toggleFormExcluir(){
+    this.displayFormExcluir = !this.displayFormExcluir;
+  }
+
+  closeFormExcluir(){
+    this.displayFormExcluir = !this.displayFormExcluir;
+  }
+
+  displayFormEditar = false;
+
+
+  toggleFormEditar(){
+    this.displayFormEditar = !this.displayFormEditar;
+  }
+
+  closeFormEditar(){
+    this.displayFormEditar = false;
+  }
+
 
   formatDate(data: any): string {
     const dateObject = (data instanceof Date) ? data : new Date(data);
@@ -21,5 +52,5 @@ export class CardTarefaComponent {
       day: '2-digit'
     });
   }
-
+  
 }
