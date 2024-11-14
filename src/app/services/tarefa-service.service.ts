877709bba9b80c7a36f49c29f381bd4c6d6fdfa4
Thullyoo <thullyofilho@gmail.com>
2024-11-14 12:13:54 -0300

    feat: adicionado reload ao concluir uma requisição

--- src/app/services/tarefa-service.service.ts
+++ src/app/services/tarefa-service.service.ts
@@ -5,6 +5,7 @@ import type { Observable } from 'rxjs';
 import type { TarefaOrdem } from '../type/tarefa-ordem-type';
 import type TarefaDTO from '../type/tarefa-dto-type';
 import { environment } from '../../enviroment/environment.prod';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -22,28 +23,40 @@ export class TarefaServiceService {
   editarOrdemTarefas(listOrdem: TarefaOrdem[]){
     const payload = { ordemList: listOrdem };
     this.httpClient.put<Tarefa[]>(this.url, payload).subscribe({
-      next: (response) => console.log("Requisição bem-sucedida", response),
+      next: (response) => {
+        console.log("Requisição bem-sucedida", response)
+        window.location.reload()
+      },
         error: (error) => console.error("Erro ao fazer a requisição", error)
     });
   }
   
   incluirTarefa(dto: TarefaDTO){
     this.httpClient.post(this.url, dto).subscribe({
-      next: (response) => console.log("Requisição bem-sucedida", response),
+      next: (response) =>{ 
+        console.log("Requisição bem-sucedida", response)
+        window.location.reload()
+      },
         error: (error) => console.error("Erro ao fazer a requisição", error)
     });
   }
 
   excluirTarefa(tarefa_id: Number){
     this.httpClient.delete(this.url + `/${tarefa_id}`).subscribe({
-      next: (response) => console.log("Requisição bem-sucedida", response),
+      next: (response) => {
+        console.log("Requisição bem-sucedida", response)
+        window.location.reload()
+      },
         error: (error) => console.error("Erro ao fazer a requisição", error)
     });
   }
 
   editarTarefa(tarefa_id: Number, dto: TarefaDTO){
     this.httpClient.put(this.url + `/${tarefa_id}`, dto).subscribe({
-      next: (response) => console.log("Requisição bem-sucedida", response),
+      next: (response) =>{
+        console.log("Requisição bem-sucedida", response)
+        window.location.reload()
+    },
         error: (error) => console.error("Erro ao fazer a requisição", error)
     });
   }
