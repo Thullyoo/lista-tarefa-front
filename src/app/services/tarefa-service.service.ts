1ffc8de42a352288d99170a3626f8bd7b173b3cf
Thullyoo <thullyofilho@gmail.com>
2024-11-14 15:11:17 -0300

    feat: adicionado janela de error

--- src/app/services/tarefa-service.service.ts
+++ src/app/services/tarefa-service.service.ts
@@ -6,6 +6,7 @@ import type { TarefaOrdem } from '../type/tarefa-ordem-type';
 import type TarefaDTO from '../type/tarefa-dto-type';
 import { environment } from '../../enviroment/environment.prod';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,8 @@ export class TarefaServiceService {
   httpClient: HttpClient = inject(HttpClient);
 
   private url: string = environment.apiUrl;
+
+  toastr = inject(ToastrService)
 
   listarTarefa(): Observable<Tarefa[]>{
     return this.httpClient.get<Tarefa[]>(this.url);
@@ -37,7 +40,10 @@ export class TarefaServiceService {
         console.log("Requisição bem-sucedida", response)
         window.location.reload()
       },
-        error: (error) => console.error("Erro ao fazer a requisição", error)
+        error: (error) => {
+          console.error("Erro ao fazer a requisição", error)
+          this.toastr.error(error.error.message)
+        }
     });
   }
 
